ffe3a8613e117d3a2d5d7bbeaa9b7b5e33f38050
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2023-06-26 18:38:48 +0530

    Rating Service Added

---
+++ RatingService/src/main/java/com/springboot/rating/services/RatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.rating.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceApplication.class, args);
+	}
+
+}
