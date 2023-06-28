eb4c7019fbfd2e64fab00331f3de77b158fac761
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2023-06-28 15:54:31 +0530

    ServiceRegistry Updated

--- ServiceRegistry/src/main/java/com/springboot/service/registry/ServiceRegistryApplication.java
+++ ServiceRegistry/src/main/java/com/springboot/service/registry/ServiceRegistryApplication.java
@@ -2,12 +2,14 @@ package com.springboot.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
 
 }
