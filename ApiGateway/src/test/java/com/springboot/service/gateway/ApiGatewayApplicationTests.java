6411f377f758060eb39b3634e575debc9c06b188
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2023-07-02 19:00:32 +0530

    Api Gateway Setup Added

---
+++ ApiGateway/src/test/java/com/springboot/service/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.service.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
