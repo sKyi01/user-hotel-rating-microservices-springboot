63e1736996103aa356223f05376e38cb274954fa
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2023-07-04 10:01:40 +0530

    ApiGateway configured and Services Added

--- ApiGateway/src/main/java/com/springboot/service/api/gateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/springboot/service/api/gateway/ApiGatewayApplication.java
@@ -1,10 +1,14 @@
-package com.springboot.service.gateway;
+package com.springboot.service.api.gateway;
+
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- ApiGateway/src/test/java/com/springboot/service/api/gateway/ApiGatewayApplicationTests.java
+++ ApiGateway/src/test/java/com/springboot/service/api/gateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springboot.service.gateway;
+package com.springboot.service.api.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
