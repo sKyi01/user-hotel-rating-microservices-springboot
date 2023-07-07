1cd77a2a4c960912ede7f53347e4d9e7a8ce39f7
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2023-07-08 01:18:00 +0530

    OKTA Security Configured for ApiGateway

---
+++ ApiGateway/src/main/java/com/springboot/service/api/gateway/config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.springboot.service.api.gateway.config;public class SecurityConfig {
+}
---
+++ ApiGateway/src/main/java/com/springboot/service/api/gateway/controller/AuthController.java
@@ -0,0 +1,2 @@
+package com.springboot.service.api.gateway.controller;public class AuthController {
+}
